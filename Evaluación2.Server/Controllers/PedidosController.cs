c31dc42eb399a34df9f383404949fb2fa99486ed
Mauro <cabrera.mauronaim@gmail.com>
2026-01-30 19:44:21 -0300

    delete controller removido

--- Evaluación2.Server/Controllers/PedidosController.cs
+++ Evaluación2.Server/Controllers/PedidosController.cs
@@ -77,58 +77,9 @@ namespace ProyectoModelado2024.Server.Controllers
             }
         }
 
-        [HttpPut("{id:int}")]
-        public async Task<ActionResult> Put(int id, [FromBody] Pedido entidad)
-        {
-            if (id != entidad.Id)
-            {
-                return BadRequest("Datos incorrectos");
-            }
-            var sel = await repositorio.SelectById(id);
-
-            if (sel == null)
-            {
-                return NotFound("No existe el tipo de documento buscado.");
-            }
-
-            //sel.Codigo = entidad.Codigo;
-            //sel.Nombre = entidad.Nombre;
-
-            sel = mapper.Map<Pedido>(entidad); //pruebo a usar el mapper aqui
-
-            try
-            {
-                await repositorio.Update(id, sel);
-                return Ok();
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
+        
 
 
-        [HttpDelete("{id:int}")]
-        public async Task<ActionResult> Delete(int id)
-        {
-            var existe = await repositorio.Existe(id);
-            if (!existe)
-            {
-                return NotFound($"El tipo de producto {id} no existe");
-            }
-            Pedido EntidadABorrar = new Pedido();
-            EntidadABorrar.Id = id;
-
-            if (await repositorio.Delete(id))
-            {
-                return Ok();
-            }
-            else
-            {
-                return BadRequest();
-            }
-
-        }
     }
 }
 
